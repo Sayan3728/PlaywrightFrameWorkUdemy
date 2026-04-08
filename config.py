55f0e0bf2f90ceee56a73eee56f7a3b6a98f0748
Sayan <sayanbcroy@gmail.com>
2026-04-08 13:14:39 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    email="sunnyhemsheela@gmail.com"
+    password="admin"
+
+    invalid_email="pavanol123@abc.com"
+    invalid_password="test@123xyz"
+
+    product_name = "MacBook"
+    product_quantity = "1"
+    total_price = "$602.00"
